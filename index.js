c2f6b551553b18d0314d3e463653926f3b21d7fe
Michal Vašíček <me@michalvasicek.cz>
2019-03-15 17:28:05 +0100

    Cleanup when disconnect

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ http.createServer(function (req, res) {
 	const queryClients = query.userIds.split(',');
 	queryClients.forEach((item) => {
 		if (typeof clients[parseInt(item)] !== 'undefined') {
-			clients[parseInt(item)].send(JSON.stringify({
-				chatId: parseInt(query.chatId),
-			}));
+			clients[parseInt(item)].forEach(client => {
+				client.send(JSON.stringify({
+					chatId: parseInt(query.chatId),
+				}));
+			});
 		}
 	});
 	res.end(JSON.stringify({"status": "ok"}));
@@ -25,10 +27,16 @@ wss.on('connection', function connection(ws) {
 	ws.on('message', function incoming(message) {
 		const parsedMessage = JSON.parse(message);
 		if (parsedMessage.type === 'start') {
-			clients[parsedMessage.data.id] = ws;
+			if (typeof clients[parsedMessage.data.id] === 'undefined') {
+				clients[parsedMessage.data.id] = [];
+			}
+			clients[parsedMessage.data.id].push(ws);
+
+			ws.on('close', function() {
+				const index = clients[parsedMessage.data.id].indexOf(this);
+				clients[parsedMessage.data.id].splice(index, 1);
+			});
 		}
 		console.log(`Message ${parsedMessage.type} with data ${JSON.stringify(parsedMessage.data)}`);
 	});
-
-	// ws.send(JSON.stringify({key: 'value'}));
 });
